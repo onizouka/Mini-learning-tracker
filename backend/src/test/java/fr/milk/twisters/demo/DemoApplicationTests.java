2efbdb63099bfc112f12dce0b77dfdaccfacf7f5
VANWIERST Julien <julien.vanwierst@externe.maif.fr>
2026-06-30 13:24:02 +0200

    initial commit

---
+++ backend/src/test/java/fr/milk/twisters/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.milk.twisters.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
