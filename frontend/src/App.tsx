207babc4cc97323b909f52f5cd982696aaf059f2
Julien Vanwierst <julien.vanwierst@gmail.com>
2026-07-03 09:28:08 +0200

    fix : frontend components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
 import './App.css'
-import { useEffect, useState} from "react";
-import type {ResourceInterface} from "./interfaces/ResourceInterface.ts";
+// import { useEffect, useState} from "react";
+// import type {ResourceInterface} from "./interfaces/ResourceInterface.ts";
+import ResourceForm from "./components/ResourceForm.tsx";
 
 function App() {
 
-  const [resourcesList, setResourcesList] = useState<ResourceInterface[]>([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/api/resources')
-    .then(res => res.json())
-    .then((data: ResourceInterface[] )=> {setResourcesList(data); console.log(data)})
-
-  }, []);
-
-  return (
-    <>
-     <h1>bonjour</h1>
-      <ul>
-        {
-        resourcesList.map((resourceItem) =>
-                <li key={resourceItem.id}>
-                  <div>{resourceItem.title}</div>
-                </li>
-          )
-        }
-
-
-      </ul>
-    </>
-  )
+  return <ResourceForm/>
+  // const [resourcesList, setResourcesList] = useState<ResourceInterface[]>([]);
+  //
+  // useEffect(() => {
+  //   fetch('http://localhost:8080/api/resources')
+  //   .then(res => res.json())
+  //   .then((data: ResourceInterface[] )=> {setResourcesList(data); console.log(data)})
+  //
+  // }, []);
+  //
+  // return (
+  //   <>
+  //    <h1>bonjour</h1>
+  //     <ul>
+  //       {
+  //       resourcesList.map((resourceItem) =>
+  //               <li key={resourceItem.id}>
+  //                 <div>{resourceItem.title}</div>
+  //               </li>
+  //         )
+  //       }
+  //
+  //
+  //     </ul>
+  //   </>
+  // )
 }
 
 export default App
